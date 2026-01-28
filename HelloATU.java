e8deec1275f792dba635596d25d8756e87c53aaf
desireep <L00193301@atu.ie>
2026-01-28 14:06:52 +0000

    Added HelloATU.java

---
+++ HelloATU.java
@@ -0,0 +1,6 @@
+public class HelloATU {
+	public static void main(String[] args) {
+	//prints "Hello, ATU from Maria" as output
+	System.out.println("Hello, ATU from Maria");
+	}
+}
---
+++ LICENCE.md
@@ -0,0 +1,4 @@
+\# this is my licence file
+
+\# this work is copyright of Desiree Payoyo
+
